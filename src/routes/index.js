d61550575d64eadc057bbc980c3c3d3ceb23cb75
Francisco <franciscoelder0@gmail.com>
2024-02-21 23:29:20 -0300

    vista productView

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,25 @@
-import webSocketRoute from "./websocket.route.js";
-import productsRoute from "./products.route.js";
-import cartsRoute from "./cart.route.js";
-import sessionRoute from "./session.route.js";
-import mockingRoute from "./mocking.route.js";
-import loggerRoute from "./logger.route.js";
+import webSocketRoute from './websocket.route.js';
+import productsRoute from './products.route.js';
+import cartsRoute from './cart.route.js';
+import sessionRoute from './session.route.js';
+import mockingRoute from './mocking.route.js';
+import loggerRoute from './logger.route.js';
 
-import api from "./api/index.js";
+import api from './api/index.js';
 
-import { Router } from "express";
+import { Router } from 'express';
+import { notFound } from '../controllers/NotFound.controller.js';
+import { accessPublicWithoutAuth, authToHome } from '../util/jwt.js';
 const router = Router();
 
-router.use("/chat", webSocketRoute);
-router.use("/cart", cartsRoute);
-router.use("/products", productsRoute);
-router.use("/", sessionRoute);
-router.use("/loggerTest", loggerRoute);
-router.use("/mockingproducts", mockingRoute);
+router.use('/chat', webSocketRoute);
+router.use('/cart', cartsRoute);
+router.use('/products', productsRoute);
+router.use('/', sessionRoute);
+router.use('/loggerTest', loggerRoute);
+router.use('/mockingproducts', mockingRoute);
 
-router.use("/api", api);
+router.use('/api', api);
 
-router.use("*", (req, res) => res.status(404).render("404"));
+router.use('*', authToHome, notFound);
 export default router;
